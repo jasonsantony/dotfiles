b9340ee29ce25a28fd8c34b8bc10875ea4a3914d
Jason Antony <jasonsantony@gmail.com>
2025-09-03 17:58:58 -0400

    opaque and jk=Esc

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,13 +2,17 @@
 vim.opt.termguicolors = true
 
 -- Transparency
-vim.cmd([[
-  augroup TransparentBackground
-    autocmd!
-    autocmd ColorScheme * highlight Normal ctermbg=none guibg=none
-    autocmd ColorScheme * highlight NonText ctermbg=none guibg=none
-  augroup END
-]])
+-- vim.cmd([[
+--   augroup TransparentBackground
+--     autocmd!
+--     autocmd ColorScheme * highlight Normal ctermbg=none guibg=none
+--     autocmd ColorScheme * highlight NonText ctermbg=none guibg=none
+--   augroup END
+-- ]])
+
+-- Esc remap
+vim.keymap.set("i", "jk", "<Esc>", { desc = "Exit insert mode with jk" })
+vim.opt.timeoutlen = 200
 
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
