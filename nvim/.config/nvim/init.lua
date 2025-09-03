2dbd9ed04521797d0c68f3117d9f0cf1bb3014ed
Jason Antony <jasonsantony@gmail.com>
2025-09-03 17:45:24 -0400

    nvim setup

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,5 +1,14 @@
 -- Enable truecolor in Neovim
 vim.opt.termguicolors = true
 
+-- Transparency
+vim.cmd([[
+  augroup TransparentBackground
+    autocmd!
+    autocmd ColorScheme * highlight Normal ctermbg=none guibg=none
+    autocmd ColorScheme * highlight NonText ctermbg=none guibg=none
+  augroup END
+]])
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
