1bd978463552aa6b2fa3826fc653e68b33fbd2ae
Jason Antony <jasonsantony@gmail.com>
2025-09-08 05:30:05 -0400

    reduce space latency

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- normal mode timeout delay
+vim.o.timeout = true
+vim.o.timeoutlen = 100
+
+-- insert mode timeout delay
+vim.o.ttimeout = true
+vim.o.ttimeoutlen = 100
