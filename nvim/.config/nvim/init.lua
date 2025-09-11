9613d200a5614f998c0c73983e8e9f9cafda0a4d
Jason Antony <jasonsantony@gmail.com>
2025-09-10 20:06:12 -0400

    remove comment

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,15 +1,6 @@
 -- Enable truecolor in Neovim
 vim.opt.termguicolors = true
 
--- Transparency
--- vim.cmd([[
---   augroup TransparentBackground
---     autocmd!
---     autocmd ColorScheme * highlight Normal ctermbg=none guibg=none
---     autocmd ColorScheme * highlight NonText ctermbg=none guibg=none
---   augroup END
--- ]])
-
 -- Esc remap
 vim.keymap.set("i", "jk", "<Esc>", { desc = "Exit insert mode with jk" })
 vim.opt.timeoutlen = 200
