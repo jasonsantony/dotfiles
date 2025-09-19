66133610decbf534d6a41a152c3c6620eeedc766
Jason Antony <jasonsantony@gmail.com>
2025-09-18 21:18:39 -0400

    idek bruh

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,12 +1,2 @@
--- transparency override
-vim.api.nvim_create_autocmd("ColorScheme", {
-    callback = function()
-        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-        vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
-    end,
-})
-
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
