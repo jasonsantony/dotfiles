409890977aa89bee88dc712a63a18a5cfb7865b2
Jason Antony <jasonsantony@gmail.com>
2025-09-24 03:47:38 -0400

    nvim transparency and file reorganization

---
+++ nvim/.config/nvim/lua/plugins/transparent.lua
@@ -0,0 +1,45 @@
+return {
+    "xiyaowong/transparent.nvim",
+    lazy = false, -- load early so it applies to highlights
+    config = function()
+        require("transparent").setup({
+            groups = { -- default groups
+                "Normal",
+                "NormalNC",
+                "Comment",
+                "Constant",
+                "Special",
+                "Identifier",
+                "Statement",
+                "PreProc",
+                "Type",
+                "Underlined",
+                "Todo",
+                "String",
+                "Function",
+                "Conditional",
+                "Repeat",
+                "Operator",
+                "Structure",
+                "LineNr",
+                "NonText",
+                "SignColumn",
+                "CursorLine",
+                "CursorLineNr",
+                "StatusLine",
+                "StatusLineNC",
+                "EndOfBuffer",
+            },
+            extra_groups = {
+                -- plugin UIs you want transparent
+                "NeoTreeNormal",
+                "NeoTreeNormalNC",
+                "NvimTreeNormal",
+                "NvimTreeNormalNC",
+                "NormalFloat",
+                "FloatBorder",
+            },
+            exclude_groups = {}, -- you can keep some groups opaque
+        })
+    end,
+}
