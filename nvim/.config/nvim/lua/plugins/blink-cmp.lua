400bcd72b19d8673fba836530f7ac4d0088cf7f7
Jason Antony <jasonsantony@gmail.com>
2026-08-15 23:46:50 -0400

    tentative blink

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -2,8 +2,9 @@ return {
   "saghen/blink.cmp",
   opts = {
     completion = {
-      menu = {
-        auto_show_delay_ms = 1000,
+      trigger = {
+        show_on_keyword = false,
+        show_on_backspace_after_insert_enter = false,
       },
       ghost_text = {
         show_without_menu = false,
